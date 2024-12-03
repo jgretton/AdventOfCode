afe75b05da98975b7473bdd639a21789e35c487a
Joshua Gretton <jb.gretton@googlemail.com>
2024-12-03 21:37:30 +0000

    Day 1 better solution
    Instead of adding the file as a JS array. I looked into how to read the text file into my code.
    
    Learnt:
    - how to read and use txt files.
    - you can deconstruct array items within the map function to return the first element of the array.
    -

--- 1/part_1.js
+++ 1/part_1.js
@@ -1,23 +1,40 @@
-import list from "./list";
-let list_1 = [];
-let list_2 = [];
-let acummulation = 0;
+// import list from "./list";
+// let list_1 = [];
+// let list_2 = [];
 
-//Map through and split initial array into 2.
-list.map((item, index) => {
-	if (index % 2) {
-		list_1.push(item);
-	} else list_2.push(item);
+// //Map through and split initial array into 2.
+// list.map((item, index) => {
+// 	if (index % 2) {
+// 		list_1.push(item);
+// 	} else list_2.push(item);
+// });
+
+// //Sort numerically
+// list_1.sort();
+// list_2.sort();
+
+// //go through each index, minus values to find the gap, math absolute to make sure its positive.
+// // add to accumulation to get final value.
+// for (let index = 0; index < list_1.length; index++) {
+// 	acummulation = acummulation + Math.abs(list_1[index] - list_2[index]);
+// }
+
+// // console.log(acummulation);
+const fs = require("fs");
+
+fs.readFile("puzzle.txt", "utf-8", (err, data) => {
+	//split each line into its own array and into its own items.
+	const arr = data.split("\n").map((row) => row.split("   "));
+
+	// Learnt that if you add an array item in the map function it uses array deconstruction to get the first element.
+	const list_1 = arr.map(([item]) => +item).sort();
+	const list_2 = arr.map(([, item]) => +item).sort();
+
+	let acummulation = 0;
+
+	for (let index = 0; index < list_1.length; index++) {
+		acummulation = acummulation + Math.abs(list_1[index] - list_2[index]);
+	}
+
+	console.log(acummulation);
 });
-
-//Sort numerically
-list_1.sort();
-list_2.sort();
-
-//go through each index, minus values to find the gap, math absolute to make sure its positive.
-// add to accumulation to get final value.
-for (let index = 0; index < list_1.length; index++) {
-	acummulation = acummulation + Math.abs(list_1[index] - list_2[index]);
-}
-
-console.log(acummulation);
--- 1/part_2.js
+++ 1/part_2.js
@@ -1,34 +1,59 @@
-import list from "./list.js";
+// import list from "./list.js";
 
-let list_1 = [];
-let list_2 = [];
-let similarity_score = 0;
+// let list_1 = [];
+// let list_2 = [];
+// let similarity_score = 0;
 
-//Map through and split initial array into 2.
-list.map((item, index) => {
-	if (index % 2) {
-		list_1.push(item);
-	} else list_2.push(item);
-});
+// //Map through and split initial array into 2.
+// list.map((item, index) => {
+// 	if (index % 2) {
+// 		list_1.push(item);
+// 	} else list_2.push(item);
+// });
 
-//Sort numerically
-list_1.sort();
-list_2.sort();
+// //Sort numerically
+// list_1.sort();
+// list_2.sort();
 
-//loop through list_1. Loop through list_2 for each value in list_1.
-// while looping, minus list_1[index] from list_2[index2] and if it = 0 then the value appears.
-// Add 1 to the sum value to show it appeared.
-// Muliply list_1 value by sum and add to similarity score.
-for (let i = 0; i < list_1.length; i++) {
-	let sum = 0;
-	for (let j = 0; j < list_2.length; j++) {
-		// list_1[i] - list_2[j] if === 0;  + 1 to sum;
-		if (list_1[i] - list_2[j] === 0) {
-			sum++;
+// //loop through list_1. Loop through list_2 for each value in list_1.
+// // while looping, minus list_1[index] from list_2[index2] and if it = 0 then the value appears.
+// // Add 1 to the sum value to show it appeared.
+// // Muliply list_1 value by sum and add to similarity score.
+// for (let i = 0; i < list_1.length; i++) {
+// 	let sum = 0;
+// 	for (let j = 0; j < list_2.length; j++) {
+// 		// list_1[i] - list_2[j] if === 0;  + 1 to sum;
+// 		if (list_1[i] - list_2[j] === 0) {
+// 			sum++;
+// 		}
+// 	}
+
+// 	similarity_score = similarity_score + list_1[i] * sum;
+// }
+
+// console.log(similarity_score);
+
+const fs = require("fs");
+
+fs.readFile("puzzle.txt", "utf-8", (err, data) => {
+	const arr = data.split("\n").map((row) => row.split("   "));
+
+	const list_1 = arr.map(([item]) => +item).sort();
+	const list_2 = arr.map(([, item]) => +item).sort();
+
+	let similarity_score = 0;
+
+	for (let i = 0; i < list_1.length; i++) {
+		let sum = 0;
+		for (let j = 0; j < list_2.length; j++) {
+			// list_1[i] - list_2[j] if === 0;  + 1 to sum;
+			if (list_1[i] - list_2[j] === 0) {
+				sum++;
+			}
 		}
+
+		similarity_score = similarity_score + list_1[i] * sum;
 	}
 
-	similarity_score = similarity_score + list_1[i] * sum;
-}
-
-console.log(similarity_score);
+	console.log(similarity_score);
+});
