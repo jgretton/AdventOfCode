bd5c144c1a93cefaa056d2297294595fa6836dc6
Joshua Gretton <jb.gretton@googlemail.com>
2024-12-04 13:23:54 +0000

    refactor part_1
    put the safe check into a single function rather than 2.
    used forEach loop to check if rows were safe.

--- 2/part_1.js
+++ 2/part_1.js
@@ -1,49 +1,29 @@
 const fs = require("fs");
+const isSafe = (row) => {
+	let isAscending = true;
+	let isDescending = true;
 
-/* 
+	for (let index = 0; index < row.length - 1; index++) {
+		const difference = Math.abs(row[index] - row[index + 1]);
 
-Arr is reading in as strings not numbers. need to parse items;
+		if (difference === 0 || difference > 3) return false;
 
-*/
-
+		if (row[index] > row[index + 1]) isAscending = false;
+		if (row[index] < row[index + 1]) isDescending = false;
+	}
+	// Only return false if it's neither ascending nor descending
+	return isAscending || isDescending;
+};
 fs.readFile("puzzle.txt", "utf-8", (err, data) => {
-	//split each line, then split each number within the array.
+	//Split each line, then split each number within the array.
 	const arr = data.split("\n").map((row) => row.split(" ").map(Number));
-	const isSorted = (arr) => {
-		// if number is al{ways negative then ascending
-		let isAscending = true;
-		let isDescending = true;
-		for (let index = 0; index < arr.length - 1; index++) {
-			if (arr[index] > arr[index + 1]) isAscending = false;
-			else if (arr[index] < arr[index + 1]) isDescending = false;
-		}
-		return isAscending || isDescending;
-		//if always positive then descending.
-	};
-	const isDiffer = (arr) => {
-		let result = true;
-		for (let index = 0; index < arr.length - 1; index++) {
-			const difference = Math.abs(arr[index] - arr[index + 1]);
-			if (difference === 0 || difference > 3) {
-				return (result = false);
-			}
-		}
-		return result;
-	};
-
 	let safe = 0;
 
-	for (let index = 0; index < arr.length; index++) {
-		// console.log(index);
-		const check_1 = isSorted(arr[index]);
-		// console.log(check_1, " - ", index + 1);
-		const check_2 = isDiffer(arr[index]);
-		// console.log("index[", index, "] - ", check_1, check_2, safe);
-		if (check_1 === true && check_2 === true) {
+	arr.forEach((row) => {
+		if (isSafe(row)) {
 			safe++;
-			// console.log("index[", index, "] - ", safe);
-			// console.log(safe);
+			return;
 		}
-	}
+	});
 	console.log(safe);
 });
